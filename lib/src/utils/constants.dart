898814ee606b0fbf188750acbdd585424317f52d
Omar Flores <dev@dev.com>
2025-04-10 08:59:09 -0700

    Add color constants for glitch effects and white color to BrutalConstants

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -5,6 +5,15 @@ class BrutalConstants {
   /// Color transparente para usar en toda la biblioteca
   static const transparent = Color(0x00000000);
 
+  /// Color rojo para efectos glitch
+  static const red = Color(0xFFFF0000);
+
+  /// Color cian para efectos glitch
+  static const cyan = Color(0xFF00FFFF);
+
+  /// Color blanco
+  static const white = Color(0xFFFFFFFF);
+
   // Prevenir instanciación
   const BrutalConstants._();
 }
